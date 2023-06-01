4eff8cba9c6ba34342378531be6983244e4bbbee
136S-Pfrimmer <136-rpfrimmer@sd43.bc.ca>
2023-06-01 15:30:42 -0700

    Development continues ( I do not remember what I have added )

--- main.py
+++ main.py
@@ -1,23 +1,35 @@
 import serial
 import time
 
-p = serial.Serial('COM4')
-time.sleep(1)
+class EMU:
+    def __init__(self):
+        self.port = ''
+        self.components = []
+        self.com = ''
+
+    def com(self,port,rate=9600):
+        self.port = port
+        self.com = serial.Serial(self.port,rate)
+        time.sleep(1) # wait for arduino to catch up TODO have arduino respond instead
+    def page(self,command: str, x):
+        self.com.write(command.encode('utf-8'))
+        self.com.write(self.id.encode('utf-8'))
+        self.com.write(x.encode('utf-8'))
 
 class Component:
-    def __init__(self, ID: str):
+    def __init__(self, ID: str, EMUInstance: EMU):
+        self.EMUInstance = EMUInstance
         self.ID = ID
         self.state = 0
     def write(self,x: int):
         self.state = x
-        self.actuate("W",x=x)
-    def actuate(self, command: str, x: int=""):
-        p.write('{COMMAND} {ID} {X}'.format(COMMAND=command, ID=self.ID, X=x).encode('utf-8'))
+        self.EMUInstance.page("W",self.ID,self.state)
 
-class DCMotor(Component):
-    def __init__(self, ID):
-        super().__init__(ID)
+class ServoMotor(Component):
+    def __init__(self, ID: str, EMUInstance: EMU):
+        super().__init__(ID, EMUInstance)
 
-m = DCMotor('MX0')
+e = EMU()
+e.com('COM5',rate=4800)
 
-m.write(90)
+ms0 = e.ServoMotor('MS0')
